4afe5d60c3a83ca4a3458e390710185f34b68c7c
tanakaworld <yutaro.tanaka.world@gmail.com>
2016-09-17 16:17:31 +0900

    Show html in markdown

--- public/tutorial.js
+++ public/tutorial.js
@@ -1,11 +1,17 @@
 const Comment = React.createClass({
+    rawMarkup() {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return {__html: rawMarkup};
+    },
+
     render() {
         return (
             <div className="comment">
                 <h2 className="commentAuthor">
                     {this.props.author}
                 </h2>
-                {this.props.children}
+                <span dangerouslySetInnerHTML={this.rawMarkup()}/>
             </div>
         )
     }
